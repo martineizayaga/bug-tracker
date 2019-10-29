0cab8576e406a5824d21457b7f8c3307784a3170
martineizayaga <martineizayaga@gmail.com>
2019-10-29 13:17:04 -0400

    added edit and delete

--- routes/index.js
+++ routes/index.js
@@ -2,54 +2,14 @@ const express = require('express');
 const { isEmpty } = require('lodash');
 const Post = require('../models/post');
 const router = express.Router();
+const posts = require('../controllers/post.controller.js');
 
-router.get('/posts', async (req, res) => {
-    try {
-        const posts = await Post.find({});
+router.get('/posts', posts.get_all);
 
-        return res.json({
-            posts
-        });
-    } catch (error) {
-        return res.status(500).json({
-            message: 'Internal Server error'
-        });
-    }
-});
+router.post('/add', posts.create);
 
-router.post('/add', async (req, res) => {
-    if (isEmpty(req.body)) {
-        return res.status(403).json({
-            message: 'Body should not be empty',
-            statusCode: 403
-        });
-    }
-    const { issue_type, summary, description, priority } = req.body;
+router.put('/post/:postId', posts.update);
 
-    const newPost = new Post({
-        issue_type,
-        summary,
-        description,
-        priority,
-        date: Date.now()
-    });
-    try {
-        await newPost.save();
-        res.json({
-            message: 'Data successfully saved',
-            statusCode: 200,
-            issue_type,
-            summary,
-            description,
-            priority
-        });
-    } catch (error) {
-        console.log('Error: ', error);
-        res.status(500).json({
-            message: 'Internal Server error',
-            statusCode: 500
-        });
-    }
-});
+router.delete('/post/:postId', posts.delete);
 
 module.exports = router;
